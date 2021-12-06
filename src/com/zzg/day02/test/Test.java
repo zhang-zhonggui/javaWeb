561081c9ab0bf385cc588a143634c7fd078b6940
2521250151 <2521250151@qq.com>
2021-12-06 16:37:59 +0800

    JavaWeb的第二天

---
+++ src/com/zzg/day02/test/Test.java
@@ -0,0 +1,29 @@
+package com.zzg.day02.test;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: zzg
+ * @Description: 今天的一次练习
+ * @DateTime: 2021/11/27 9:58
+ */
+public class Test extends Test2Servlet{
+
+    @Override
+    public void service(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+    @Override
+    public void doPost(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public void doGet(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+}
---
+++ src/com/zzg/day02/test/Test2Servlet.java
@@ -0,0 +1,39 @@
+package com.zzg.day02.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: zzg
+ * @Description: TODO
+ * @DateTime: 2021/11/27 9:58
+ */
+public class Test2Servlet extends Test1Servlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        service(req, resp);
+    }
+
+    public void service(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
+        String method = servletRequest.getMethod();
+        if (method.equals("GET")) {
+            doPost(servletRequest,servletResponse);
+        }else if(method.equals("POST")){
+            doGet(servletRequest,servletResponse);
+        }
+    }
+    public void  doPost(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println(1);
+    }
+    public void  doGet(HttpServletRequest servletRequest, HttpServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println(2);
+    }
+
+
+}
