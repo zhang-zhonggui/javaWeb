bc9baf89558566e3a8221561b59c5258387f30dd
张中贵 <89518224+zhang-zhonggui@users.noreply.github.com>
2021-12-10 09:39:01 +0800

    Create DAOUtil.java

--- src/com/zzg/day03/util/DAOUtil.java
+++ src/com/zzg/day03/util/DAOUtil.java
@@ -8,16 +8,18 @@ import java.util.Map;
 
 /**
  * @Author: zzg
- * @Description: TODO
+ * @Description: 将公共部分进行封装
  * @DateTime: 2021/11/27 9:58
  */
 public class DAOUtil {
+    //设置常量用来消除魔法值
     private static final String CLASS_NAME_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/hehe";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "1012";
     static {
         try {
+            //加载驱动
             Class.forName(CLASS_NAME_DRIVER);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -25,9 +27,10 @@ public class DAOUtil {
     }
 
     public static Connection getConnection() throws SQLException {
+        //连接数据库
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
-
+//获取传入的数据，然后对数据库进行操作，返回List<Map>的数据
     public static List<Map> request(String sql, Object... obj) {
         Connection conn = null;
         try {
@@ -60,6 +63,7 @@ public class DAOUtil {
         }
         return null;
     }
+    //获取传入的数据，然后对数据库进行增删改操作，返回int数据判断数据是否成功
     public static int update(String sql, Object... obj) {
         Connection conn = null;
         try {
